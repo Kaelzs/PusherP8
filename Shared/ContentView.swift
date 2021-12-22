db5c36061defffbf5296ab32513d4e385cc0b954
Kael <Kael.zs17@gmail.com>
2021-12-22 18:42:08 +0800

    feat: some logic update

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -24,6 +24,36 @@ struct TitleAndTextField: View {
     }
 }
 
+struct TokenKey: Hashable {
+    let keyID: String
+    let teamID: String
+    let p8: String
+}
+
+class TokenStorage {
+    var tokens: [TokenKey: (String, Date)] = [:]
+
+    func token(forKeyID keyID: String, teamID: String, p8: String) throws -> String {
+        let tokenKey = TokenKey(keyID: keyID, teamID: teamID, p8: p8)
+        if let exist = tokens[tokenKey] {
+            let expiredDate = exist.1
+            if expiredDate.timeIntervalSinceNow > 0 {
+                return exist.0
+            }
+        }
+
+        let now = Date()
+
+        let jwt = JWT(keyID: keyID, teamID: teamID, issueDate: now, expireDuration: 60 * 60)
+        let token = try jwt.sign(with: p8)
+
+        tokens[tokenKey] = (token, now.addingTimeInterval(60 * 60))
+        return token
+    }
+}
+
+let tokenStorage = TokenStorage()
+
 struct ContentView: View {
     @AppStorage("deviceToken") var deviceToken: String = ""
     @AppStorage("keyID") var keyID: String = ""
@@ -34,6 +64,23 @@ struct ContentView: View {
     @AppStorage("production") var production: Bool = true
 
     @State var textViewErrorMessage: String = ""
+    enum SendingStates {
+        case sending, successful(String), failed(String), none
+
+        var text: Text {
+            switch self {
+            case .sending:
+                return Text("sending").foregroundColor(.primary)
+            case .successful(let string):
+                return Text(string.isEmpty ? "successful" : "successful: \(string)").foregroundColor(.green)
+            case .failed(let string):
+                return Text("failed: \(string)").foregroundColor(.red)
+            case .none:
+                return Text("")
+            }
+        }
+    }
+    @State var sendingStates: SendingStates = .none
 
     var body: some View {
         let content = VStack {
@@ -59,6 +106,8 @@ struct ContentView: View {
             HStack {
                 Toggle("production", isOn: $production)
                 Spacer()
+                sendingStates.text
+                Spacer()
                 Button("Send") {
                     self.sendRequest()
                 }
@@ -75,10 +124,17 @@ struct ContentView: View {
     }
 
     func sendRequest() {
-        let jwt = JWT(keyID: keyID, teamID: teamID, issueDate: Date(), expireDuration: 60 * 60)
-        guard let token = try? jwt.sign(with: p8) else {
+        sendingStates = .sending
+
+        let token: String
+        do {
+            token = try tokenStorage.token(forKeyID: keyID, teamID: teamID, p8: p8)
+        } catch {
+            print(error.localizedDescription)
+            sendingStates = .failed(error.localizedDescription)
             return
         }
+
         var request = URLRequest(url: URL(string: (production ? "https://api.push.apple.com:443/3/device/" : "https://api.sandbox.push.apple.com:443/3/device/") + deviceToken)!)
         request.httpMethod = "POST"
         request.addValue("bearer \(token)", forHTTPHeaderField: "authorization")
@@ -88,14 +144,24 @@ struct ContentView: View {
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard error == nil else {
                 print(error!.localizedDescription)
+                sendingStates = .failed(error!.localizedDescription)
                 return
             }
+
             guard let data = data else {
                 print("Empty data")
+                sendingStates = .failed("no response data")
                 return
             }
-            if let str = String(data: data, encoding: .utf8) {
-                print(str)
+
+            let statusCode = (response as? HTTPURLResponse)?.statusCode ?? 0
+            let str = String(data: data, encoding: .utf8) ?? ""
+
+            print("\(statusCode): \(str)")
+            if (200 ..< 300).contains(statusCode) {
+                sendingStates = .successful(str)
+            } else {
+                sendingStates = .failed("\(statusCode): \(str)")
             }
         }.resume()
     }
